e1b6fa9fac072925c112bba1340c864ae31497a1
SSPatil4 <sspatil4official@gmail.com>
2023-09-11 15:19:57 +0530

    new line added

--- StaticCretion/program.java
+++ StaticCretion/program.java
@@ -6,6 +6,7 @@ public class program {
     program(){
        a++;
         b++;
+
     }
     public static void main(String[] args) {
         program obj1 = new program();
@@ -17,5 +18,6 @@ public class program {
         program obj3 = new program();
         System.out.println("print instance variable a "+obj3.a++);
         System.out.println("print static variable b "+b++);
+        System.out.println("hello git hub");
     }
 }
